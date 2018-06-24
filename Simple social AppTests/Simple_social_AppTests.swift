37558cc22445e3ff0a28ebf9d96e5fee00cee525
Hadeer Kamel <hadeerkamel@Hadeers-Mac.local>
2018-06-24 23:04:58 +0200

    Initial Commit

---
+++ Simple social AppTests/Simple_social_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  Simple_social_AppTests.swift
+//  Simple social AppTests
+//
+//  Created by Hadeer Kamel on 6/24/18.
+//  Copyright © 2018 Hadeer. All rights reserved.
+//
+
+import XCTest
+@testable import Simple_social_App
+
+class Simple_social_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
